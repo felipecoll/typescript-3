6ee84f11df980d03dbc8c345f0383ecbcd795476
Felipe Coll <leonfelipecoll@gmail.com>
2025-04-30 22:26:35 -0300

    Unknown type

---
+++ src/03-unknow.ts
@@ -0,0 +1,24 @@
+let anyvar: any;
+anyvar = 1;
+anyvar = '1';
+anyvar = true;
+anyvar = null
+anyvar = undefined;
+
+let isNew: boolean = anyvar;
+
+let unknowVar: unknown;
+unknowVar = 1;
+unknowVar = '1';
+unknowVar = true;
+unknowVar = null
+unknowVar = undefined;
+
+if (typeof unknowVar === 'string') {
+   unknowVar.toUpperCase();
+}
+
+const parse = (str: string) => {
+   return JSON.parse(str);
+}
+const user = parse('{"name": "John"}');
