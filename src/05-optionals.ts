00c1f31532260b994c16261a21c881c0b5963ba2
Felipe Coll <leonfelipecoll@gmail.com>
2025-05-01 20:43:24 -0300

    Parametros por defecto

--- src/05-optionals.ts
+++ src/05-optionals.ts
@@ -1,8 +1,16 @@
 export const createProduct = (
   id: string | number,
-  isNew: boolean,
+  isNew?: boolean,
   stock?: number,
-)=> { return { id, stock, isNew } }
+)=> { return
+  { id,
+    stock: stock || 10,
+    isNew: isNew || true
+  }
+}
 
 const p1 = createProduct('1', true, 10)
-console.log(p1) // { id: '1', stock: 10, isNew: true }
+console.log(p1)
+
+const p2 = createProduct(1)
+console.log(p2)// { id: '1', stock: 10, isNew: true }
