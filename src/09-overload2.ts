4d4416b846c7fd9ba0f30e229e703ee1b3a53322
Felipe Coll <leonfelipecoll@gmail.com>
2025-05-03 17:35:27 -0300

    Sobrecarga de funciones - solucion

---
+++ src/09-overload2.ts
@@ -0,0 +1,30 @@
+export function parseStr(input: string): string[];
+export function parseStr(input: string[]): string;
+export function parseStr(input: number): boolean;
+
+
+export function parseStr(input: unknown): unknown {
+  if (Array.isArray(input)) {
+    return input.join(''); // string
+  } else if (typeof input === 'string'){
+    return input.split(''); // string[]
+  } else if (typeof input === 'number'){
+    return true; // boolean
+  }
+}
+
+const rtaArray = parseStr('Nico');
+rtaArray.reverse();
+// if (Array.isArray(rtaArray)) {
+//   rtaArray.reverse();
+// }
+console.log('rtaArray', 'Nico =>' ,rtaArray);
+
+const rtaStr = parseStr(['N','i','c','o']);
+rtaStr.toLowerCase();
+// if (typeof rtaStr === 'string') {
+//   rtaStr.toLowerCase();
+// }
+console.log('rtaStr', "['N','i','c','o'] =>",rtaStr);
+
+const rtaBoolean = parseStr(12);
